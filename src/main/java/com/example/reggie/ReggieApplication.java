8b161c449378baf80a4bf60de736eaa949e7a496
Yvette <Yvettehuwonderland@163.com>
2024-03-10 21:52:45 +0800

    done

--- src/main/java/com/example/reggie/ReggieApplication.java
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -1,15 +1,19 @@
 package com.example.reggie;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
+@EnableSwagger2
+@EnableKnife4j
 public class ReggieApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/reggie/config/WebConfig.java
+++ src/main/java/com/example/reggie/config/WebConfig.java
@@ -2,11 +2,18 @@ package com.example.reggie.config;
 
 import com.example.reggie.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.List;
 
@@ -19,6 +26,8 @@ public class WebConfig extends WebMvcConfigurationSupport {
         log.info("开始静态资源映射");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+       registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         super.addResourceHandlers(registry);
     }
 
@@ -28,5 +37,26 @@ public class WebConfig extends WebMvcConfigurationSupport {
         messageConverter.setObjectMapper(new JacksonObjectMapper());
         converters.add(0,messageConverter);
     }
+
+    @Bean
+    public Docket createRestApi() {
+        //文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.blog.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
+    }
+
+
 
 }
